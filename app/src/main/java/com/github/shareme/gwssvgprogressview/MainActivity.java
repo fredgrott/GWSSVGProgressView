e4bfe65fff87816a64fb72d5cae778dc8467f3bb
Fred Grott <fred.grott@gmail.com>
2015-10-13 10:10:01 -0500

    added demo apk

--- app/src/main/java/com/github/shareme/gwssvgprogressview/MainActivity.java
+++ app/src/main/java/com/github/shareme/gwssvgprogressview/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import com.grottworkshop.gwssvgprogressviewdemo.adapter.FillablePagesAdapter;
+import com.github.shareme.gwssvgprogressview.adapter.FillablePagesAdapter;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
